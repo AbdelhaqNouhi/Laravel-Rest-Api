e2e85b77bf3cf2de50136fed9a52bcac27754431
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2023-06-02 17:19:21 +0100

    upload image

--- app/Http/Controllers/V1/ImageManipulationController.php
+++ app/Http/Controllers/V1/ImageManipulationController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Models\ImageManipulation;
 use App\Http\Requests\ResizeImageRequest;
 use App\Models\Album;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
 class ImageManipulationController extends Controller
 {
@@ -27,8 +30,40 @@ class ImageManipulationController extends Controller
      */
     public function resize(ResizeImageRequest $request)
     {
-        //
+        $all = $request->all();
+
+        $image = $all['image'];
+        unset($all['image']);
+        $data = [
+            'type' => ImageManipulation::TYPE_RESIZE,
+            'data' => json_encode($all),
+            'user_id' => null,
+        ];
+
+        if(isset($all['album_id'])) {
+            $data['album_id'] = $all['album_id'];
+        }
+
+        $dir = 'images/' . Str::random() . '/';
+        $absolutePath = public_path($dir);
+        File::makeDirectory($absolutePath);
+
+        if($image instanceof UploadedFile) {
+            $data['name'] = $image->getClientOriginalName();
+            $filename = pathinfo($data['name'], PATHINFO_FILENAME);
+            $extension = $image->getClientOriginalExtension();
+
+            $image->move($absolutePath, $data['name']);
+        } else {
+            $data['name'] = pathinfo($image, PATHINFO_BASENAME);
+            $filename = pathinfo($image, PATHINFO_FILENAME);
+            $extension = pathinfo($image, PATHINFO_EXTENSION);
+
+            copy($image, $absolutePath . $data['name']);
+        }
+        $data['path'] = $dir . $data['name'];
     }
+
 
     /**
      * Display the specified resource.
